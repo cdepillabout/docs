e9f432967261fd63e5b15ef6c525f2c0ac8379ca
(cdep) illabout <cdep.illabout@gmail.com>
2009-09-02 15:57:21 -0400

    Added new file for running command after sourcing .bashrc.  This is used with cron.

---
+++ code/source_bashrc_and_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# This file sources .bashrc and runs its arguments.
+# This is used to run files from cron that require specific
+# envvars to be set.
+
+# This is actually really confusing.  My .bashrc tests if the shell is
+# interactive, and if it is not, it just exits.  So we cannot simply do 
+# source ~/.bashrc, but we must run it under an interactive bash process.
+# Then we run the command passed in on the command line.
+bash -i -c "source ~/.bashrc; \"$@\""
+
