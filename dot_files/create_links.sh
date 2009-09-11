3bca8ba74b011cf2999c3e0cd07d224afcf678ca
(cdep) illabout <cdep.illabout@gmail.com>
2009-09-10 23:28:36 -0400

    Changed to update links in a different fashon.  Also added stuff for xmonad.

--- dot_files/create_links.sh
+++ dot_files/create_links.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 # Create the proper links from ~/ to my dot files.
 
-for dot_file in dot_* 
-do
-	ln -sf `pwd`/${dot_file} ~/.${dot_file#dot_}
-done
+source $HOME/docs/code/lib/library.sh
+
+function super_link {
+	file_name=$1
+	link_target=$2
+
+	[ ! -e $link_target -o -L $link_target ] || \
+		die "ERROR! $link_target is not a link, so exiting."
+
+	ln -sf $file_name $link_target
+}
+
+function make_directory {
+	directory=$1
+
+	if [ ! -e $directory ]
+	then
+		mkdir $directory
+	else
+		[ -d $directory ] || die "Error! $directory is not a directory"
+	fi
+}
+
+
+super_link $HOME/docs/dot_files/dot_bashrc $HOME/.bashrc
+super_link $HOME/docs/dot_files/dot_emacs $HOME/.emacs
+super_link $HOME/docs/dot_files/dot_screenrc $HOME/.screenrc
+super_link $HOME/docs/dot_files/dot_vimrc $HOME/.vimrc
+
+make_directory $HOME/.xmonad
+super_link $HOME/docs/dot_files/dot_xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs
+super_link $HOME/docs/dot_files/dot_xmonad/conky-rc $HOME/.xmonad/conky-rc
+
+#for dot_file in dot_* 
+#do
+#	ln -sf `pwd`/${dot_file} ~/.${dot_file#dot_}
+#done
 
