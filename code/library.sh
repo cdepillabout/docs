99718411d5dbcd4019a781656560cb01c11f2567
dgosnell <dgosnell@arch-linux-server.localdomain>
2009-12-31 15:43:11 -0500

    added method to make debugging easier

--- code/library.sh
+++ code/library.sh
@@ -16,3 +16,20 @@ function is_a_num {
 		echo "no"
 	fi
 }
+
+
+# This can be used for debugging output.
+# To turn on debugging, use this command:
+# _DEBUG="on"
+# To turn off debuggin, use this command:
+# _DEBUG="off"
+# It is used like so:
+# DEBUG echo "here is some debugging output."
+function DEBUG()
+{
+	# The && and || have the same precedence, and are evaluated from
+	# right to left. $@ expands to all the args passed to this function, 
+	# so namely the command you want to run. : always evaluates to 0 (true).  
+	# I'm not sure why you need this?
+	[[ "$_DEBUG" == "on" || "$_DEBUG" == "yes" ]] &&  $@ || :
+}
