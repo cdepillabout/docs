fd27714c62de6901a6e93b99ba44fc014403870b
(cdep)illabout <cdepillabout@gmail.com>
2010-07-17 15:27:23 -0400

    Updated to make the code a little cleaner.

--- code/pyscanki.py
+++ code/pyscanki.py
@@ -68,7 +68,7 @@ def check_japanese_attr(node):
     #            raise Exception
 
     # Here's probably the best way to do it:
-    if get_attribute(node, 'language') != 'ja':
+    if xpath_one_node(node, "@language") != 'ja':
         print >> sys.stderr, "node: " + str(node) + " does not have a 'ja' language attribute."
         print >> sys.stderr, debug_node(node)
         print_stack()
@@ -78,10 +78,12 @@ def xpath(node, xpath_expression):
     """Do xpath expression with a default 'vocab' namespace."""
     return node.xpath(xpath_expression, namespaces={'vocab':VOCAB_NAMESPACE})
 
-def xpath_one_node(node, xpath_expression, must_exist=True):
+def xpath_one_node(node, xpath_expression, must_exist=True, warn_if_no=False):
     """Do xpath expression on node, we are only expecting one node on result. 
     Error if more than one node.  If must_exist is True, then we error if there
-    is no result."""
+    is no result. Return None if must_exist is not true and there is no result.
+    If warn_if_no is set, then it returns a warning if it does not exist or 
+    if there is nothing there."""
     node_list = xpath(node, xpath_expression)
 
     # we will error out if must_exist is set, but there is no node
@@ -95,55 +97,24 @@ def xpath_one_node(node, xpath_expression, must_exist=True):
     # if must_exist is False and there is no node, then we can just
     # return None.  This is okay.
     if must_exist == False and (len(node_list) == 0):
-        return None
+        if warn_if_no:
+            return "WARNING, NOTHING FOUND!"
+        else:
+            return ""
 
     # Otherwise, return the node we found.
     return node_list[0]
 
-def get_attribute(node, attribute):
-    """Get attribute from a node.  We assume that the attribute exists.
-    Error if we can't find it."""
-    if (not node.attrib.has_key(attribute)):
-        print >> sys.stderr, "node: " + str(node) + " does not have attribute \"" + \
-                attribute + "\"."
-        print >> sys.stderr, debug_node(node)
-        print_stack()
-        IPSHELL()
-    return node.attrib[attribute]
-
-def get_text(node, must_contain_text=True):
-    """Get text from node. If must_contain_text is true, then we error out
-    if it doesn't contain any text."""
-    if must_contain_text and not node.text:
-        print >> sys.stderr, "No text for node: " + str(node)
-        print >> sys.stderr, debug_node(node)
-        print_stack()
-        IPSHELL()
-    return node.text
-
-def get_text_xpath(node, xpath, must_exist=True, must_contain_text=True):
-    """Get text inside node matched by xpath.  If must_exist is True, then 
-    we error out if this does not exist.  If must_contain_text is True, then
-    we error out if this does not contain text."""
-    xpath_node = xpath_one_node(node, xpath, must_exist)
-    if xpath_node is not None:
-        return get_text(xpath_node, must_contain_text)
-    else:
-        return ""
-
 def process_transliterations(transliterations_node):
-
-    hira_transliteration = get_text_xpath(transliterations_node, 
-            "vocab:transliteration[@type='Hira']", must_exist=False)
-    hrkt_transliteration = get_text_xpath(transliterations_node, 
-            "vocab:transliteration[@type='Hrkt']")
+    hira_transliteration = xpath_one_node(transliterations_node, 
+            "vocab:transliteration[@type='Hira']/text()", must_exist=False)
+    hrkt_transliteration = xpath_one_node(transliterations_node, 
+            "vocab:transliteration[@type='Hrkt']/text()")
 
     # We will have a problem if hrkt transliteration is emtpy,
     # or if hira transliteration is not empty, but it is not equal
     # to hrkt transliteration.
-    if hrkt_transliteration == "" or \
-            (hira_transliteration != "" and \
-            hira_transliteration != hrkt_transliteration):
+    if hrkt_transliteration == "":
         print >> sys.stderr, "Error! hrkt_transliteration is " + \
                 hrkt_transliteration
         print >> sys.stderr, "hira_transliteration is " + hira_transliteration
@@ -154,18 +125,16 @@ def process_transliterations(transliterations_node):
 
     return hrkt_transliteration
 
-
 def process_cue(cue_node):
     check_japanese_attr(cue_node)
 
     # it is unfortunate that there is both a <text> tag, and a TEXT part of xml.
     # This makes the below code kind of confusing.
     # Also, will part_of_speech ever be empty??
-    part_of_speech = get_attribute(cue_node, "part_of_speech")
-    text = get_text_xpath(cue_node, "vocab:text")
-    character = get_text_xpath(cue_node, "vocab:character", must_exist=False)
-    sound = get_text_xpath(cue_node, "vocab:sound", must_exist=False,
-            must_contain_text=False)
+    part_of_speech = xpath_one_node(cue_node, "@part_of_speech")
+    text = xpath_one_node(cue_node, "vocab:text/text()")
+    character = xpath_one_node(cue_node, "vocab:character/text()", must_exist=False)
+    sound = xpath_one_node(cue_node, "vocab:sound/text()", must_exist=False)
     transliteration = process_transliterations(
             xpath_one_node(cue_node, "vocab:transliterations"))
 
@@ -177,34 +146,40 @@ def process_cue(cue_node):
         print_stack()
         IPSHELL()
 
-
-    print "cue: " + character + "\t" + text + "\t" + part_of_speech + \
-            "\t" + sound + "\t" + transliteration
+    print "\tcue:"
+    print "\t\tcharacter: " + character.encode('utf-8')
+    print "\t\ttext: " + text.encode('utf-8')
+    print "\t\tpart_of_speech: " + part_of_speech
+    print "\t\tsound: " + sound 
+    print "\t\ttransliteration: " + transliteration.encode('utf-8')
 
 def process_response(responses_node):
-    english_meaning = get_text_xpath(responses_node, 
-            "vocab:response[@type='meaning'][@language='en']/vocab:text")
-    print "responses: " + english_meaning 
+    english_meaning = xpath_one_node(responses_node, 
+            "vocab:response[@type='meaning'][@language='en']/vocab:text/text()")
+    print "\tresponses: "
+    print "\t\tresponse: " + english_meaning 
 
 def process_sentence(sentence_node):
     check_japanese_attr(sentence_node)
 
-    sentence = get_text_xpath(sentence_node, "vocab:text")
-    image = get_text_xpath(sentence_node, "vocab:image", must_exist=False, 
-            must_contain_text=False)
-    sound = get_text_xpath(sentence_node, "vocab:sound", must_exist=False,
-            must_contain_text=False)
+    sentence = xpath_one_node(sentence_node, "vocab:text/text()")
+    image = xpath_one_node(sentence_node, "vocab:image/text()", must_exist=False) 
+    sound = xpath_one_node(sentence_node, "vocab:sound/text()", must_exist=False)
     transliteration = process_transliterations(
             xpath_one_node(sentence_node, "vocab:transliterations"))
-    translation = get_text_xpath(sentence_node, 
-            "vocab:translations/vocab:sentence[@language='en']/vocab:text")
+    translation = xpath_one_node(sentence_node, 
+            "./vocab:translations/vocab:sentence[@language='en']/vocab:text/text()",
+            warn_if_no=True, must_exist=False)
 
-    print "sentence: " + unicode(sentence) + "\t" + str(image) + "\t" + \
-            str(sound) + "\t" + unicode(transliteration) + "\t" + translation
-
+    print "\t\tsentence:"
+    print "\t\t\tsentence: " + sentence.encode('utf-8')
+    print "\t\t\timage: " + str(image)
+    print "\t\t\tsound: " + str(sound) 
+    print "\t\t\ttransliteration: " + transliteration.encode('utf-8')
+    print "\t\t\ttranslation: " + translation
 
 def process_sentences(sentences_node):
-    sentences = xpath(sentences_node, "vocab:sentence")
+    sentences = xpath(sentences_node, "./vocab:sentence")
 
     if len(sentences) < 1:
         print >> sys.stderr, "Error! No sentences!"
@@ -212,6 +187,7 @@ def process_sentences(sentences_node):
         print_stack()
         IPSHELL()
 
+    print "\tsentenses:"
     for sentence in sentences:
         process_sentence(sentence)
 
@@ -220,6 +196,7 @@ def process_item(item_node):
     assuming there are two sentences."""
     check_japanese_attr(item_node)
 
+    print "item:"
     process_cue(xpath_one_node(item_node, "vocab:cue"))
     process_response(xpath_one_node(item_node, "vocab:responses"))
     process_sentences(xpath_one_node(item_node, "vocab:sentences"))
@@ -244,7 +221,9 @@ def main():
         from IPython.Shell import IPShellEmbed
         IPSHELL = IPShellEmbed([])
 
-    list = 19053 
+    #list = 19053  # core2k
+    #list = 24532   # core6k
+    list = 71776   # ko2001 
     url = "http://api.smart.fm/lists/" + str(list) + "/items.xml?per_page=20"
 
     tree = etree.parse(url)
