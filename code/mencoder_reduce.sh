e18e0e547b75c272b96fe490e51702555d344b11
(cdep)illabout <cdepillabout@gmail.com>
2010-07-29 00:29:02 -0400

    Added code to change the file extension and video codec.

--- code/mencoder_reduce.sh
+++ code/mencoder_reduce.sh
@@ -4,6 +4,19 @@
 
 source ~/docs/code/library.sh
 
+# takes two numbers as arguments and says whether they are close
+# or not (based on some allowed difference).  So a sample function
+# call could be `close_to 1.1 1.2 0.1`, which would return "yes"
+function close_to()
+{
+	num_a=$1
+	num_b=$2
+	allowed_difference=$3
+
+	difference=`echo "x = $num_a - $num_b ; if (0 > x) x *= -1; x" | bc -l`
+	echo $difference $allowed_difference | awk '{if ($1 <= $2) print "yes";}'
+}
+
 RATIO_4_3="$(echo "4 / 3" | bc -l)"
 RATIO_16_9="$(echo "16 / 9" | bc -l)"
 
@@ -21,21 +34,9 @@ input_movie_width="$(echo "$full_ratio_string" | grep -Po "\d{3,}(x|X)" | grep -
 input_movie_height="$(echo "$full_ratio_string" | grep -Po "(x|X)\d{3,}" | grep -Po "\d{3,}")"
 input_movie_ratio="$(echo "${input_movie_width} / ${input_movie_height}" | bc -l)"
 
-# takes two numbers as arguments and says whether they are close
-# or not (based on some allowed difference).  So a sample function
-# call could be `close_to 1.1 1.2 0.1`, which would return "yes"
-function close_to()
-{
-	num_a=$1
-	num_b=$2
-	allowed_difference=$3
 
-	difference=`echo "x = $num_a - $num_b ; if (0 > x) x *= -1; x" | bc -l`
-	echo $difference $allowed_difference | awk '{if ($1 <= $2) print "yes";}'
-}
-
-close_to $input_movie_ratio $RATIO_4_3 0.1
-close_to $input_movie_ratio $RATIO_16_9 0.1
+#close_to $input_movie_ratio $RATIO_4_3 0.1
+#close_to $input_movie_ratio $RATIO_16_9 0.1
 
 # get the new reduced video ratio (based on the old one)
 if [[ `close_to $input_movie_ratio $RATIO_4_3 0.1` ]] ; then
@@ -50,7 +51,50 @@ else
 	die "Error! Could not find the correct ratio for the input file $input_movie"
 fi
 
-new_file_name="$(echo "$input_movie" | sed -e 's/[0-9]\{3,\}\([Xx]\)[0-9]\{3,\}/'${new_file_width}'\1'${new_file_height}'/')"
+new_file_name="$(echo "$input_movie" | \
+	sed -e 's/[0-9]\{3,\}\([Xx]\)[0-9]\{3,\}/'${new_file_width}'\1'${new_file_height}'/')"
+
+function get_codec 
+{
+	input_movie="$1"
+
+	echo "$input_movie" | \
+		grep -Po "\(\d{3,}(x|X)\d{3,} .*?\)" | \
+		sed -r 's/\([0-9]{3,}(x|X)[0-9]{3,} (.*?)\)/\2/'
+}
+
+# get the old codec and replace with the new codec
+function change_codec 
+{
+	input_movie="$1"
+	new_codec="$2"
+
+	old_codec="$(get_codec "$input_movie")"
+
+	if [ -n "$old_codec" ]
+	then
+		echo "$input_movie" | \
+			sed -r 's/\(([0-9]{3,}(X|x)[0-9]{3,}) '"$old_codec"'\)/\(\1 '"$new_codec"'\)/'
+	else
+		echo "$input_movie"
+	fi
+
+}
+
+new_file_name="$(change_codec "$new_file_name" "FMP4")"
+
+function change_extension
+{
+	input_movie="$1"
+	extensionless_input_movie="${input_movie%.*}"
+	new_extension="$2"
+
+	echo "${extensionless_input_movie}.${new_extension}"
+}
+
+new_file_name="$(change_extension "$new_file_name" "avi")"
+
+# replace the extension with .avi
 
 DEBUG echo "new file name is ${new_file_name}. starting encoding..."
 
