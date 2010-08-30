6b94319c38465942586d8329d40cd3a19d0844ac
(cdep)illabout <cdepillabout@gmail.com>
2010-08-30 01:24:34 -0400

    Added vimperatorrc

--- dot_files/create_links.sh
+++ dot_files/create_links.sh
@@ -60,3 +60,5 @@ make_directory $HOME/.vim/after/syntax
 super_link $HOME/docs/dot_files/dot_vim/after/syntax/sas.vim $HOME/.vim/after/syntax/sas.vim
 super_link $HOME/docs/dot_files/dot_vim/after/syntax/java.vim $HOME/.vim/after/syntax/java.vim
 
+# vimperator
+super_link $HOME/docs/dot_files/dot_vimperatorrc $HOME/.vimperatorrc
