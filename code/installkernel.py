a46380755ac9d85066dc2b891597c78b808d0007
(cdep) illabout <cdep.illabout@gmail.com>
2011-02-14 01:28:52 -0500

    Updated to also install modules.

--- code/installkernel.py
+++ code/installkernel.py
@@ -33,43 +33,83 @@ def debug_print_cur_ids():
     gname = grp.getgrgid(filestat.st_gid).gr_name
     print ("file stat: %s -- (%s, %s)\n" % (name, uname, gname))
     os.close(fd)
+    os.remove(name)
+
+def run_command(command, dropprivs=True, replaceusergroup=False):
+    """
+    Run `command`. Drop privs to user running sudo if dropprivs is True.
+    Replace strings SUDOUSER and SUDOGROUP with the user running sudo
+    if replaceusergroup is True.
+    """
+    pid = os.fork()
+    if not pid:
+        owner_uid = os.stat(".").st_uid     # owner of the kernel sources
+        original_uid = os.getuid()
+        sudo_uid_gid_exist = ("SUDO_UID" in os.environ and \
+                "SUDO_GID" in os.environ)
+
+        if replaceusergroup and not sudo_uid_gid_exist:
+            sys.stderr.write("ERROR: trying to replace sudo user and group "
+                    "in command `%s`, but this command is not being run "
+                    "with SUDO_UID and SUDO_GID enviroment variables "
+                    "available.\n" % command)
+            sys.exit(1)
+
+        if replaceusergroup:
+            sudo_uid = int(os.environ["SUDO_UID"])
+            sudo_gid = int(os.environ["SUDO_GID"])
+            sudo_user_name = pwd.getpwuid(sudo_uid).pw_name
+            sudo_group_name = grp.getgrgid(sudo_gid).gr_name
+            command = command.replace("SUDOUSER", sudo_user_name)
+            command = command.replace("SUDOGROUP", sudo_group_name)
+
+        if sudo_uid_gid_exist and \
+                (owner_uid != original_uid) and (dropprivs == True):
+            # we are being run in sudo, so we need to drop privs to run make
+            sudo_uid = int(os.environ["SUDO_UID"])
+            sudo_gid = int(os.environ["SUDO_GID"])
+            os.setgid(sudo_gid)
+            os.setuid(sudo_uid)
+
+
+        # need to get this 
+        cur_user_name = pwd.getpwuid(os.getuid()).pw_name
+
+        print("Running `%s`..." % command)
+        retcode = subprocess.call(command, shell=True, stdout=sys.stdout, stderr=sys.stderr)
+        if retcode < 0:
+            sys.stderr.write("ERROR: `%s` was terminated by signal %s.\n" % 
+                    (command, -retcode))
+            sys.exit(1)
+        elif retcode > 0:
+            sys.stderr.write("ERROR: `%s` failed while "
+                    "running as user %s.\n" % (command, cur_user_name))
+            sys.exit(1)
+        print "Finished `%s`.\n" % command
+        sys.exit(0)
+    else:
+        waitpid, exit_status = os.wait()
+        if exit_status != 0:
+            sys.exit(1)
 
 def main():
     parser = argparse.ArgumentParser(description="Install the current kernel.")
     parser.add_argument('--make', '-m', action='store_true', 
             help="run `make` first")
+    parser.add_argument('--make-install-modules', '-i', action='store_true', 
+            help="run `make modules_install`")
     args = parser.parse_args()
 
     goto_ksrc_root()
 
     if args.make:
+        run_command("make")
 
-        pid = os.fork()
-        if not pid:
-            owner_uid = os.stat(".").st_uid     # owner of the kernel sources
-            original_uid = os.getuid()
+    if args.make_install_modules:
+        run_command("make modules_install", dropprivs=False)
+        run_command("chown -R SUDOUSER:SUDOGROUP .", dropprivs=False,
+                replaceusergroup=True)
 
-            if "SUDO_UID" in os.environ and "SUDO_GID" in os.environ and owner_uid != original_uid:
-                # we are being run in sudo, so we need to drop privs to run make
-                sudo_uid = int(os.environ["SUDO_UID"])
-                sudo_gid = int(os.environ["SUDO_GID"])
-                os.setgid(sudo_gid)
-                os.setuid(sudo_uid)
-
-            print "Running `make`..."
-            retcode = subprocess.call("make", shell=True, stdout=sys.stdout, stderr=sys.stderr)
-            if retcode < 0:
-                sys.stderr.write("ERROR: `make` was terminated by signal %s.\n" % -retcode)
-                sys.exit(1)
-            elif retcode > 0:
-                sys.stderr.write("ERROR: `make` failed.\n")
-                sys.exit(1)
-            print "Finished `make`.\n"
-            sys.exit(0)
-        else:
-            waitpid, exit_status = os.wait()
-            if exit_status != 0:
-                sys.exist(1)
 
     # make sure you can write to boot
     if not os.access("/boot", os.W_OK):
