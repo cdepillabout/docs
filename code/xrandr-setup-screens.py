a5dd72a0503659a2e8fc40e157d47087d10a9d25
cdep.illabout <cdep.illabout@gmail.com>
2013-05-01 22:31:58 +0900

    xrandr-setup-screens broke when system was upgraded, so this is fixing it.

--- code/xrandr-setup-screens.py
+++ code/xrandr-setup-screens.py
@@ -15,22 +15,29 @@ DEBUG = nodebug
 
 # regex strings for checking which monitors are active.
 # These may have to be changed if I get a new monitor or tv or something."
-LCD_ACTIVE_REGEX = 'LVDS-1 connected 1920x1080+0+600 (normal left inverted right x axis y axis) 344mm x 193mm\n   1920x1080      60.0*+   60.0     50.0'
-EXTERNAL_MONITOR_ACTIVE_REGEX = 'DVI-I-1 connected 1080x1920+1920+0 left (normal left inverted right x axis y axis) 510mm x 287mm\n   1920x1080      60.0*+   50.0'
-TV_ACTIVE_REGEX = 'HDMI-1 connected 1920x1080+0+600 (normal left inverted right x axis y axis) 886mm x 498mm\n   1920x1080      60.0*+   30.0     24.0     30.0'
+LCD_ACTIVE_REGEX = 'LVDS-1 connected (primary )?1920x1080\+0\+600 \(normal left inverted right x axis y axis\) 344mm x 193mm\n   1920x1080      60\.0\*\+   60\.0     50\.0'
+EXTERNAL_MONITOR_ACTIVE_REGEX = 'DVI-I-1 connected (primary )?1080x1920\+1920\+0 left \(normal left inverted right x axis y axis\) 510mm x 287mm\n   1920x1080      60.0\*\+   50.0'
+TV_ACTIVE_REGEX = 'HDMI-1 connected (primary )?1920x1080\+0\+600 \(normal left inverted right x axis y axis\) 886mm x 498mm\n   1920x1080      60.0\*\+   30.0     24.0'
 
 XRANDR_LCD_AND_EXTERN_MON_COMMAND = 'xrandr --fb 3000x1920 --output HDMI-1 --off && xrandr --fb 3000x1920 --output LVDS-1 --auto --primary --pos 0x600 --output DVI-I-1 --rotate left  --pos 1920x0 --mode "1920x1080"'
 XRANDR_TV_AND_EXTERN_MON_COMMAND = 'xrandr --fb 3000x1920 --output LVDS-1 --off && xrandr --fb 3000x1920 --output HDMI-1 --auto --primary --pos 0x600 --output DVI-I-1 --rotate left  --pos 1920x0 --mode "1920x1080"'
 
-def setup_lcd_and_extern_mon():
+def setup_lcd_and_extern_mon(dryrun):
     DEBUG("Using xrandr to setup LCD and external monitor with this command:")
     DEBUG("\t'%s'" % XRANDR_LCD_AND_EXTERN_MON_COMMAND)
-    Popen(XRANDR_LCD_AND_EXTERN_MON_COMMAND, shell=True)
+    if dryrun:
+        DEBUG("Not run because only dry-run.");
+    else:
+        Popen(XRANDR_LCD_AND_EXTERN_MON_COMMAND, shell=True)
 
-def setup_tv_and_extern_mon():
+
+def setup_tv_and_extern_mon(dryrun):
     DEBUG("Using xrandr to setup TV and external monitor with this command:")
     DEBUG("\t'%s'" % XRANDR_TV_AND_EXTERN_MON_COMMAND)
-    Popen(XRANDR_TV_AND_EXTERN_MON_COMMAND, shell=True)
+    if dryrun:
+        DEBUG("Not run because only dry-run.");
+    else:
+        Popen(XRANDR_TV_AND_EXTERN_MON_COMMAND, shell=True)
 
 def runcommand(args):
     """
@@ -55,6 +62,8 @@ def main():
     parser.add_argument('--startup', '-s', action='store_true', default=False,
             help="setup LCD screen with external monitor")
     parser.add_argument('--debug', '-d', action='store_true', default=False,
+            help="print debug messages")
+    parser.add_argument('--dry-run', '-n', action='store_true', default=False,
             help="print debug messages")
     args = parser.parse_args()
 
@@ -83,7 +92,7 @@ def main():
 
     # find out if lcd is active
     lcd_active = False
-    if re.search(re.escape(LCD_ACTIVE_REGEX), out):
+    if re.search(LCD_ACTIVE_REGEX, out):
         lcd_active = True
         DEBUG("found lcd active")
     else:
@@ -91,7 +100,7 @@ def main():
 
     # find out if tv is active
     tv_active = False
-    if re.search(re.escape(TV_ACTIVE_REGEX), out):
+    if re.search(TV_ACTIVE_REGEX, out):
         tv_active = True
         DEBUG("found tv active")
     else:
@@ -99,7 +108,7 @@ def main():
 
     # find out if external monitor is active
     extern_mon_active = False
-    if re.search(re.escape(EXTERNAL_MONITOR_ACTIVE_REGEX), out):
+    if re.search(EXTERNAL_MONITOR_ACTIVE_REGEX, out):
         extern_mon_active = True
         DEBUG("found external monitor active")
     else:
@@ -107,15 +116,15 @@ def main():
 
     if args.startup:
         if extern_mon_avail:
-            setup_lcd_and_extern_mon()
+            setup_lcd_and_extern_mon(args.dry_run)
         else:
             DEBUG("called with --startup, but external monitor is not available, so not doing anything")
         sys.exit(0)
 
     if lcd_active == True and tv_avail == True and tv_active == False and extern_mon_avail == True and extern_mon_active == True:
-        setup_tv_and_extern_mon()
+        setup_tv_and_extern_mon(args.dry_run)
     elif lcd_active == False and tv_avail == True and tv_active == True and extern_mon_avail == True and extern_mon_active == True:
-        setup_lcd_and_extern_mon()
+        setup_lcd_and_extern_mon(args.dry_run)
     else:
         DEBUG("monitors are in weird state, so not doing aything.  try running with --startup flag.")
 
