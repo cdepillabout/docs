3a52150a47565dffd1b8cccb3d3c49be3dc9fe62
(cdep)illabout <cdep.illabout@gmail.com>
2015-03-18 22:27:41 +0900

    Updated append_to_path bash function to be able to force appending to the path.

--- code/library.sh
+++ code/library.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 #these are functions and vars for bash scripts
 
 
@@ -15,7 +15,7 @@ MAGENTA_TEXT='\E[35m'
 CYAN_TEXT='\E[36m'
 WHITE_TEXT='\E[37m'
 
-# bold color output 
+# bold color output
 BOLD_TEXT='\E[01m'
 BOLD_BLACK_TEXT="${BOLD_TEXT}${BLACK_TEXT}"
 BOLD_RED_TEXT="${BOLD_TEXT}${RED_TEXT}"
@@ -56,8 +56,8 @@ function is_a_num {
 function DEBUG()
 {
 	# The && and || have the same precedence, and are evaluated from
-	# right to left. $@ expands to all the args passed to this function, 
-	# so namely the command you want to run. : always evaluates to 0 (true).  
+	# right to left. $@ expands to all the args passed to this function,
+	# so namely the command you want to run. : always evaluates to 0 (true).
 	# I'm not sure why you need this?
 	[[ "$_DEBUG" == "on" || "$_DEBUG" == "yes" ]] &&  $@ || :
 }
@@ -103,78 +103,127 @@ function get_correct_filename_from_choices()
 	done
 }
 
-# Add a path to a $*PATH enviroment variable.  This works for $PATH,
+# Add a path to a $*PATH environment variable.  This works for $PATH,
 # $PYTHONPATH, $CLASSPATH, etc.  It only adds the path if it is not already
 # present.  Also, it checks to make sure this is a valid path before it
 # is added.
 #
-# $1 is the path enviroment variable to use, "PYTHONPATH", "PATH", etc.
+# $1 is the path environment variable to use, "PYTHONPATH", "PATH", etc.
 # $2 is the path to add.
 # $3 is "back" if you want to add this path to the end, and "front" if you want to
 #	 add this path to the beginning.
-# $4 whether or not to check if the directory exists
+# $4 whether or not to check if the directory exists.  Should be
+#    "yes" or "no".
+# $5 whether or not to force adding the directory to the path even
+#    if it is already on the path.  Should be "yes" or "no".  If
+#    $4 is "yes", and this is "yes", throw an error.
 function add_to_path_side()
 {
 	envvar="$1"
 	currentval="$(eval "echo $(echo '$'$envvar)")"
-	path="${2}"
+	path="$2"
 	side="$3"
 	checkifexists="$4"
+	force="$5"
 
 	# remove the trailing '/' from $path, if it exists.
 	path="${path%'/'}"
 
-	if [ -z "${envvar}" -o -z "${path}" -o -z "${side}" -o -z "${checkifexists}" ] ; then
+	# make sure that $envvar, $path, $side, $checkifexists, and $force are set.
+	if [ -z "${envvar}" -o -z "${path}" ] ; then
 		echo "ERROR! There was an empty value passed to add_to_path_side."
 		echo "Your add_to_path or prepend_to_path statement is missing an argument."
 		return
 	fi
 
-	if [[ "$checkifexists" = "yes" && ! -d "$path" ]] ; then
+	# make sure that $side is set to "front" or "back"
+	if [ "${side}" != "front" -a "${side}" != "back" ] ; then
+		echo "ERROR! side is not equal to \"front\" or \"back\" in a call to add_to_path_side."
+		return
+	fi
+
+	# make sure that $checkifexists is set to "yes" or "no"
+	if [ "${checkifexists}" != "yes" -a "${checkifexists}" != "no" ] ; then
+		echo "ERROR! checkifexists is not equal to \"yes\" or \"no\" in a call to add_to_path_side."
+		return
+	fi
+
+	# make sure that $force is set to "yes" or "no"
+	if [ "${force}" != "yes" -a "${force}" != "no" ] ; then
+		echo "ERROR! force is not equal to \"yes\" or \"no\" in a call to add_to_path_side."
+		return
+	fi
+
+	# make sure that $checkifexists and $force are not both set to "yes"
+	if [ "${checkifexists}" = "yes" -a "${force}" = "yes" ] ; then
+		echo "ERROR! checkifexists and force are both set to \"yes\" in a call to add_to_path_side."
+		return
+	fi
+
+	# Don't do anything is $checkifexists is "yes", force is "no",
+	# and the $path doesn't exist.
+	if [[ "${checkifexists}" = "yes" && ! -d "${path}" && "${force}" = "no" ]] ; then
 		#echo "not adding ${path} to "'$'"${envvar} because it is not a valid path" >&2
 		return
 	fi
 
-	if [[ ":${currentval}:" = *":${path}:"* ]] ; then
+	# Don't add the $path if it already exists in PATH, unless force is "yes".
+	if [[ ":${currentval}:" = *":${path}:"* && "${force}" = "no" ]] ; then
 		#echo "not adding ${path} to "'$'"${envvar} because it already is in "'$'"${envvar}" >&2
 		return
 	fi
 
-	if [[ "$side" = "front" || "$side" = "beginning" ]] ; then
+	# If $path already exists in PATH and $force is "yes", then remove
+	# $path from PATH so we can add it later.  It will only show up once.
+	if [[ ":${currentval}:" = *":${path}:"* && "${force}" = "yes" ]] ; then
+		remove_from_path "${envvar}" "${path}"
+	fi
+
+	if [[ "${side}" = "front" ]] ; then
 		export "$envvar"="${path}:${currentval}"
-	elif [[ "$side" = "back" || "$side" = "end" ]] ; then
+	elif [[ "${side}" = "back" ]] ; then
 		export "$envvar"="${currentval}:${path}"
 	fi
 }
 
 function append_to_path()
 {
-	add_to_path_side "$1" "$2" "back" "yes"
+	add_to_path_side "$1" "$2" "back" "yes" "no"
 }
 function add_to_path()
 {
 	echo "WARNING: add_to_path has been deprecated in favor of append_to_path."
-	add_to_path_side "$1" "$2" "back" "yes"
+	add_to_path_side "$1" "$2" "back" "yes" "no"
 }
 
 function append_to_path_nocheck()
 {
-	add_to_path_side "$1" "$2" "back" "no"
+	add_to_path_side "$1" "$2" "back" "no" "no"
 }
 function add_to_path_nocheck()
 {
 	echo "WARNING: add_to_path_nocheck has been deprecated in favor of append_to_path_nocheck."
-	add_to_path_side "$1" "$2" "back" "no"
+	add_to_path_side "$1" "$2" "back" "no" "no"
+}
+
+function append_to_path_force()
+{
+	add_to_path_side "$1" "$2" "back" "no" "yes"
 }
 
 function prepend_to_path()
 {
-	add_to_path_side "$1" "$2" "front" "yes"
+	add_to_path_side "$1" "$2" "front" "yes" "no"
 }
 
 function prepend_to_path_nocheck()
 {
-	add_to_path_side "$1" "$2" "front" "no"
+	add_to_path_side "$1" "$2" "front" "no" "no"
+}
+
+function prepend_to_path_force()
+{
+	add_to_path_side "$1" "$2" "front" "no" "yes"
 }
 
 # Remove a path from a $*PATH enviroment variable.  This works for $PATH,
