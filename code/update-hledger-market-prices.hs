4439bfcc05fabd1ef956ee8613e3b4eeb3cd55eb
(cdep)illabout <cdep.illabout@gmail.com>
2016-07-04 13:35:55 +0900

    Adding file to update market prices in hledger format.

---
+++ code/update-hledger-market-prices.hs
@@ -0,0 +1,9 @@
+#!/usr/bin/env stack
+-- stack --resolver nightly-2016-07-03 --install-ghc runghc --package yahoo-finance-api --package classy-prelude
+
+{-# LANGUAGE NoImplicitPrelude #-}
+
+import ClassyPrelude
+
+main :: IO ()
+main = undefined
