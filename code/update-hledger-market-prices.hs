c7a648ffcb51ccb9fd0105951c55187770c93bab
(cdep)illabout <cdep.illabout@gmail.com>
2016-07-04 15:18:03 +0900

    Updating hledger market prices code.

--- code/update-hledger-market-prices.hs
+++ code/update-hledger-market-prices.hs
@@ -9,9 +9,10 @@
 import ClassyPrelude
 
 import Control.Monad.Except ( runExceptT )
-import Data.ConfigFile ( ConfigParser, CPError, CPErrorData, emptyCP, readfile, to_string )
+import Data.ConfigFile
+    ( ConfigParser, CPError, CPErrorData, emptyCP, get, readfile, to_string )
 import Data.Text ( replace )
-import System.Directory ( getHomeDirectory )
+import System.Directory ( getHomeDirectory, listDirectory )
 import System.Exit ( exitFailure )
 import System.IO.Unsafe ( unsafePerformIO )
 import Text.Shakespeare.Text ( st )
@@ -20,6 +21,7 @@ import Web.Yahoo.Finance ( getQuote )
 configFilePath :: FilePath
 configFilePath = unpack configFilePathText
 
+-- | Full path to the config rc file.
 configFilePathText :: Text
 configFilePathText = replaceTildeWithHomeDir "~/.update-hledger-market-prices.rc"
 
@@ -33,40 +35,54 @@ that looks like the following:
 market-prices-dir = ~/some/path/to/market-prices
 ``` |]
 
+-- | Die with the error message.
 die :: forall a . Text -> IO a
 die msg = do
     putStrLn $ "ERROR! " <> msg
     putStrLn dieMsg
     exitFailure
 
+-- | Get the market prices directory from the config file.
 getMarketPricesDir :: IO FilePath
 getMarketPricesDir = do
-    (val :: Either CPError ConfigParser) <-
+    (eitherConfigParser :: Either CPError ConfigParser) <-
         handle couldNotReadConfFileHandler $ readfile emptyCP configFilePath
-    configParser <- either noParseError pure val
-    putStrLn $ pack $ to_string configParser
-    -- get configParser "DEFAULT" 
-    undefined
+    configParser <- either noParseError pure eitherConfigParser
+    -- putStrLn $ pack $ to_string configParser
+    let eitherMarketPricesDir = get configParser "DEFAULT" "market-prices-dir"
+    either noMarketPricesDir pure eitherMarketPricesDir
   where
     couldNotReadConfFileHandler :: forall a . SomeException -> IO a
-    couldNotReadConfFileHandler _ = do
-        let couldNotReadConfFileMsg = "Could not read config file at \""
-                <> configFilePathText <> "\"."
-        die couldNotReadConfFileMsg
+    couldNotReadConfFileHandler _ = die $ "Could not read config file at \""
+        <> configFilePathText <> "\"."
 
     noParseError :: forall a . (CPErrorData, String) -> IO a
-    noParseError (cpErrorData, string) = do
-        let couldNotParseConfFileMsg = "Could not parse config file at \""
-                <> configFilePathText <> "\".\n"
-                <> tshow cpErrorData <> "\n"
-                <> "Location: " <> pack string
-        die couldNotParseConfFileMsg
+    noParseError (cpErrorData, string) = die $
+        "Could not parse config file at \"" <> configFilePathText <> "\".\n"
+            <> tshow cpErrorData <> "\n" <> "Location: " <> pack string
 
+    noMarketPricesDir :: forall a . (CPErrorData, String) -> IO a
+    noMarketPricesDir _ = die $
+        "Cannot read \"market-prices-dir\" option in config file at \""
+            <> configFilePathText <> "\"."
+
+-- | Replace all occurances of @~@ in the input 'Text' with the user's current
+-- HOME directory.
 replaceTildeWithHomeDir :: Text -> Text
 replaceTildeWithHomeDir = replace "~" (pack $ unsafePerformIO getHomeDirectory)
 
+replaceTildeWithHomeDirString :: String -> String
+replaceTildeWithHomeDirString =
+    unpack . replaceTildeWithHomeDir . pack
+
+-- | Get all the market price files from the market price file directory.
+getMarketPriceFiles :: FilePath -> IO [FilePath]
+getMarketPriceFiles marketPricesDir =
+    filter (isSuffixOf ".ledger") <$> listDirectory marketPricesDir
+
 main :: IO ()
 main = do
-    getMarketPricesDir
-    undefined
+    marketPricesDir <- replaceTildeWithHomeDirString <$> getMarketPricesDir
+    marketPriceFiles <- getMarketPriceFiles marketPricesDir
+    print marketPriceFiles
 
