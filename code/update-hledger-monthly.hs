b3abf525936635226376cb7b3993c6df7a32918c
(cdep)illabout <cdep.illabout@gmail.com>
2017-01-05 22:44:40 -0500

    Update update-hledger-monthly.

--- code/update-hledger-monthly.hs
+++ code/update-hledger-monthly.hs
@@ -1,11 +1,12 @@
 #!/usr/bin/env stack
--- stack --verbose --resolver lts-7.14 --install-ghc --no-system-ghc runghc --package classy-prelude --package hledger-lib --package hledger --package pretty-simple --package from-sum --package optparse-applicative
+-- stack --verbose --resolver lts-7.14 --install-ghc --no-system-ghc runghc --package classy-prelude --package hledger-lib --package hledger --package pretty-simple --package from-sum --package optparse-applicative -- -Wall -fwarn-incomplete-uni-patterns -fwarn-incomplete-record-updates -fwarn-monomorphism-restriction
 
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -21,8 +22,13 @@ import Hledger.Cli.CliOptions (CliOpts(..))
 import Hledger.Cli.Main (argsToCliOpts)
 import Hledger.Cli.Utils (withJournalDo)
 import Hledger.Data.Dates (getCurrentDay)
-import Hledger.Data.Types (Journal, Transaction)
-import Hledger.Reports.ReportOptions (ReportOpts, queryFromOptsOnly)
+import Hledger.Data.Amount (num)
+import Hledger.Data.Posting (nullposting)
+import Hledger.Data.Transaction (nulltransaction)
+import Hledger.Data.Types
+       (AccountName, Amount, Journal, MixedAmount(Mixed), Posting(..),
+        Quantity, Transaction(..))
+import Hledger.Reports.ReportOptions (queryFromOptsOnly)
 import Options.Applicative
        (Parser, ParserInfo, argument, execParser, fullDesc, header,
         helper, info, metavar, progDesc, str)
@@ -30,38 +36,8 @@ import System.Environment (lookupEnv)
 import System.Exit (exitFailure)
 import Text.Parsec
        (Parsec, ParseError, (<?>), char, digit, getInput, many1, noneOf,
-        optionMaybe, parse, parserFail, spaces)
-import qualified Text.Parsec as Parsec
+        parse, parserFail, spaces)
 import Text.Pretty.Simple (pPrint)
-
-data SimpleEntry = SimpleEntry
-  { amount :: PositiveInt
-  , description :: Text
-  } deriving (Data, Eq, Generic, Read, Show, Typeable)
-
-data SimpleEntriesOnDate = SimpleEntriesOnDate
-  { day :: Day
-  , simpleEntries :: [SimpleEntry]
-  } deriving (Data, Eq, Generic, Read, Show, Typeable)
-
-data DatedEntry = DatedEntry
-  { day :: Day
-  , amount :: PositiveInt
-  , description :: Text
-  } deriving (Data, Eq, Generic, Read, Show, Typeable)
-
-simpleEntryToDatedEntry :: Day -> SimpleEntry -> DatedEntry
-simpleEntryToDatedEntry day SimpleEntry {amount, description} =
-  DatedEntry day amount description
-
-simpleEntriesOnDateToDatedEntries :: SimpleEntriesOnDate -> [DatedEntry]
-simpleEntriesOnDateToDatedEntries SimpleEntriesOnDate {day, simpleEntries} =
-  simpleEntryToDatedEntry day <$> simpleEntries
-
-die :: MonadIO m => Text -> m a
-die msg = do
-  putStrLn $ "ERROR: " <> msg
-  liftIO exitFailure
 
 getHledgerCliOptsWithJournal :: MonadIO m => m CliOpts
 getHledgerCliOptsWithJournal = do
@@ -79,23 +55,112 @@ main = do
   datedInputEntries <- readDatedEntries options
   withJournalDo hledgerCliOpts (what datedInputEntries)
 
-similarTransaction :: Day -> ReportOpts -> Journal -> Text -> Maybe Transaction
-similarTransaction day reportOpts journal description =
-  let query = queryFromOptsOnly day reportOpts
-      historyMatches = transactionsSimilarTo journal query description
-      bestMatch = listToMaybe historyMatches
-  in fmap snd bestMatch
-
 getSimilarTransaction :: Day -> CliOpts -> Journal -> Text -> Maybe Transaction
 getSimilarTransaction today hledgerCliOpts journal desc =
   let reportOpts = reportopts_ hledgerCliOpts
-      maybeTrans = similarTransaction today reportOpts journal "breakfast"
-  in maybeTrans
+      query = queryFromOptsOnly today reportOpts
+      historyMatches = transactionsSimilarTo journal query desc
+      bestMatch = listToMaybe historyMatches
+  in fmap snd bestMatch
 
 what :: MonadIO m => [DatedEntry] -> CliOpts -> Journal -> m ()
-what inputEntries hledgerCliOpts journal = do
-  pPrint inputEntries
-  undefined
+what datedEntries hledgerCliOpts journal = do
+  today <- liftIO getCurrentDay
+  let datedEntriesAndMaybeSimilarTransactions =
+        zipDatedEntryAndSimilarTransaction today hledgerCliOpts journal <$>
+        datedEntries
+      datedEntriesAndMaybeNewTransactions =
+        fmap
+          (uncurry zipDatedEntryAndNewTransaction)
+          datedEntriesAndMaybeSimilarTransactions
+      datedEntriesAndTransactions =
+        fmap (uncurry nothingTransactionToTempTransaction) datedEntriesAndMaybeNewTransactions
+  pPrint datedEntriesAndTransactions
+
+nothingTransactionToTempTransaction :: DatedEntry -> Maybe Transaction -> (DatedEntry, Transaction)
+nothingTransactionToTempTransaction datedEntry (Just transaction) = (datedEntry, transaction)
+nothingTransactionToTempTransaction datedEntry Nothing = (datedEntry, createTempTransaction datedEntry)
+
+zipDatedEntryAndSimilarTransaction :: Day
+                                   -> CliOpts
+                                   -> Journal
+                                   -> DatedEntry
+                                   -> (DatedEntry, Maybe Transaction)
+zipDatedEntryAndSimilarTransaction today hledgerCliOpts journal =
+  applyFuncAndZip $
+  datedEntryToMaybeSimilarTransaction today hledgerCliOpts journal
+
+zipDatedEntryAndNewTransaction :: DatedEntry
+                               -> Maybe Transaction
+                               -> (DatedEntry, Maybe Transaction)
+zipDatedEntryAndNewTransaction datedEntry Nothing = (datedEntry, Nothing)
+zipDatedEntryAndNewTransaction datedEntry (Just similarTransaction) =
+  ( datedEntry
+  , datedEntryAndSimilarTransactionToRealTransaction
+      datedEntry
+      similarTransaction)
+
+datedEntryToMaybeSimilarTransaction :: Day
+                                    -> CliOpts
+                                    -> Journal
+                                    -> DatedEntry
+                                    -> Maybe Transaction
+datedEntryToMaybeSimilarTransaction today hledgerCliOpts journal datedEntry =
+  let entryDesc = (description :: DatedEntry -> Text) datedEntry
+  in getSimilarTransaction today hledgerCliOpts journal entryDesc
+
+datedEntryAndSimilarTransactionToRealTransaction :: DatedEntry
+                                                 -> Transaction
+                                                 -> Maybe Transaction
+datedEntryAndSimilarTransactionToRealTransaction datedEntry similarTransaction =
+  case paccount <$> tpostings similarTransaction of
+    [account1, account2] -> do
+      assetsCashAccount <- findAssetsCashAccountName account1 account2
+      expensesAccount <- findExpensesAccountName account1 account2
+      pure $ createNewTransaction datedEntry assetsCashAccount expensesAccount
+    _ -> Nothing
+
+createNewTransaction :: DatedEntry -> AccountName -> AccountName -> Transaction
+createNewTransaction datedEntry assetsCashAccount expensesAccount =
+  let entryAmount = datedEntryAmountToQuantity datedEntry
+      expensesAmount = num entryAmount
+      assetsCashAmount = num (negate entryAmount)
+      expensesPosting =
+        accountAndAmountToPosting expensesAccount expensesAmount
+      assetsCashPosting =
+        accountAndAmountToPosting assetsCashAccount assetsCashAmount
+  in nulltransaction
+     { tdate = (day :: DatedEntry -> Day) datedEntry
+     , tdescription = (description :: DatedEntry -> Text) datedEntry
+     , tpostings = [assetsCashPosting, expensesPosting]
+     }
+
+createTempTransaction :: DatedEntry -> Transaction
+createTempTransaction datedEntry =
+  let transaction = createNewTransaction datedEntry "assets:cash" "expenses:_"
+  in transaction {tcomment = "TODO this transaction"}
+
+positiveIntToQuantity :: PositiveInt -> Quantity
+positiveIntToQuantity = fromIntegral . unPositiveInt
+
+datedEntryAmountToQuantity :: DatedEntry -> Quantity
+datedEntryAmountToQuantity =
+  positiveIntToQuantity . (amount :: DatedEntry -> PositiveInt)
+
+accountAndAmountToPosting :: AccountName -> Amount -> Posting
+accountAndAmountToPosting accountName amount =
+  nullposting {paccount = accountName, pamount = Mixed [amount]}
+
+findAssetsCashAccountName :: AccountName -> AccountName -> Maybe AccountName
+findAssetsCashAccountName "assets:cash" _ = Just "assets:cash"
+findAssetsCashAccountName _ "assets:cash" = Just "assets:cash"
+findAssetsCashAccountName _ _ = Nothing
+
+findExpensesAccountName :: AccountName -> AccountName -> Maybe AccountName
+findExpensesAccountName account1 account2
+  | "expenses" `isPrefixOf` account1 = Just account1
+  | "expenses" `isPrefixOf` account2 = Just account2
+  | otherwise = Nothing
 
 --------------------------------
 -- Parse Command Line Options --
@@ -169,21 +234,21 @@ simpleEntryParser today = do
   where
     simpleEntryParser' :: MyParser SimpleEntry
     simpleEntryParser' = do
-      positiveInt <- positiveIntParser
+      posInt <- positiveIntParser
       spaces
       desc <- many1 $ noneOf "\n\r"
       spaces
-      let simpleEntry = SimpleEntry positiveInt $ pack desc
+      let simpleEntry = SimpleEntry posInt $ pack desc
       pure simpleEntry
 
 dayParser :: Day -> MyParser Day
 dayParser today = do
   PositiveInt month <- positiveIntParser
-  char '/'
+  void $ char '/'
   PositiveInt day <- positiveIntParser
   let year = calcYear today month
       dayString = show year <> "-" <> pad month <> "-" <> pad day
-      maybeDay = parseTimeM True defaultTimeLocale "%F" dayString
+      (maybeDay :: Maybe Day) = parseTimeM True defaultTimeLocale "%F" dayString
   fromMaybeOrM maybeDay $
     parserFail $ "Could not parse \"" <> dayString <> "\" as a Day."
   where
@@ -216,15 +281,27 @@ positiveIntParser = do
 eitherToMaybe :: Either e a -> Maybe a
 eitherToMaybe = either (const Nothing) Just
 
+die :: MonadIO m => Text -> m a
+die msg = do
+  putStrLn $ "ERROR: " <> msg
+  liftIO exitFailure
+
+applyFuncAndZip :: (a -> b) -> a -> (a, b)
+applyFuncAndZip f a = (a, f a)
+
+-----------
+-- Types --
+-----------
+
 -- | Newtype wrapper for 'Int's that are above 0.
 newtype PositiveInt = PositiveInt
-  { unPositiveIntText :: Int
+  { unPositiveInt :: Int
   } deriving (Data, Eq, Generic, Read, Show, Typeable)
 
 -- | Read in a 'PositiveInt' from a 'Text'.
 positiveInt :: Text -> Either Text PositiveInt
 positiveInt inputText =
-  let maybeInt = readMay inputText
+  let (maybeInt :: Maybe Int) = readMay inputText
   in case maybeInt of
        Nothing ->
          Left $ "Cannot read input \"" <> inputText <> "\" as a positive int."
@@ -234,3 +311,27 @@ positiveInt inputText =
            else Left $
                 "input \"" <> inputText <>
                 "\" is less than 0. Not a positive int."
+
+data SimpleEntry = SimpleEntry
+  { amount :: PositiveInt
+  , description :: Text
+  } deriving (Data, Eq, Generic, Read, Show, Typeable)
+
+data SimpleEntriesOnDate = SimpleEntriesOnDate
+  { day :: Day
+  , simpleEntries :: [SimpleEntry]
+  } deriving (Data, Eq, Generic, Read, Show, Typeable)
+
+data DatedEntry = DatedEntry
+  { day :: Day
+  , amount :: PositiveInt
+  , description :: Text
+  } deriving (Data, Eq, Generic, Read, Show, Typeable)
+
+simpleEntryToDatedEntry :: Day -> SimpleEntry -> DatedEntry
+simpleEntryToDatedEntry day SimpleEntry {amount, description} =
+  DatedEntry day amount description
+
+simpleEntriesOnDateToDatedEntries :: SimpleEntriesOnDate -> [DatedEntry]
+simpleEntriesOnDateToDatedEntries SimpleEntriesOnDate {day, simpleEntries} =
+  simpleEntryToDatedEntry day <$> simpleEntries
