aac3a62731287633e7bb31c987e3ceaf28e801ec
(cdep)illabout <cdep.illabout@gmail.com>
2017-01-09 10:36:27 -0500

    Add some documentation to update-hledger-monthly.

--- code/update-hledger-monthly.hs
+++ code/update-hledger-monthly.hs
@@ -39,6 +39,8 @@ import Text.Parsec
        (Parsec, ParseError, (<?>), char, digit, getInput, many1, noneOf,
         parse, parserFail, spaces)
 
+-- | Look for the @HLEDGER_DEFAULT_LEDGER@ environment variable, and set it as
+-- the 'file_' field in a default set of 'CliOpts'.
 getHledgerCliOptsWithJournal :: MonadIO m => m CliOpts
 getHledgerCliOptsWithJournal = do
   maybeDefaultLedger <- liftIO $ lookupEnv "HLEDGER_DEFAULT_LEDGER"
@@ -53,9 +55,18 @@ main = do
   hledgerCliOpts <- getHledgerCliOptsWithJournal
   options <- parseCmdLineOptions
   datedInputEntries <- readDatedEntries options
-  withJournalDo hledgerCliOpts (what datedInputEntries)
+  withJournalDo hledgerCliOpts (writeTransactions datedInputEntries)
 
-getSimilarTransaction :: Day -> CliOpts -> Journal -> Text -> Maybe Transaction
+-- | Get the most recent 'Transaction' that is the most similar to the
+-- description.
+--
+-- Returns 'Nothing' if there are no similar transactions
+getSimilarTransaction
+  :: Day -- ^ Today
+  -> CliOpts
+  -> Journal
+  -> Text -- ^ Description to use to search in the journal.
+  -> Maybe Transaction
 getSimilarTransaction today hledgerCliOpts journal desc =
   let reportOpts = reportopts_ hledgerCliOpts
       query = queryFromOptsOnly today reportOpts
@@ -63,8 +74,15 @@ getSimilarTransaction today hledgerCliOpts journal desc =
       bestMatch = listToMaybe historyMatches
   in fmap snd bestMatch
 
-what :: MonadIO m => [DatedEntry] -> CliOpts -> Journal -> m ()
-what datedEntries hledgerCliOpts journal = do
+-- | Take a list of 'DatedEntry's, figure out similar 'Transaction's for each,
+-- replace the 'tdescription' to the 'description' of the 'DatedEntry', and
+-- write those new 'Transaction's to the 'Journal' 'file_'.
+--
+-- If we can't figure out a similar 'Transaction' (or the similar 'Transaction'
+-- doesn't meet some requirements), then just output a dummy @TODO@
+-- 'Transaction'.
+writeTransactions :: MonadIO m => [DatedEntry] -> CliOpts -> Journal -> m ()
+writeTransactions datedEntries hledgerCliOpts journal = do
   today <- liftIO getCurrentDay
   let transactions =
         snd <$>
@@ -75,6 +93,13 @@ what datedEntries hledgerCliOpts journal = do
           datedEntries
   traverse_ (liftIO . journalAddTransaction journal hledgerCliOpts) transactions
 
+-- | Take a list of 'DatedEntry's and turn them into a list of 'Transaction's
+-- to write into the 'Journal'.
+--
+-- Turn a 'DatedEntry' into a 'Transaction' by finding a similar 'Transaction'.
+-- If there are no similar 'Transaction's (or there are other errors with the
+-- similar 'Transaction'), then turn the 'DatedEntry' into a temporary @TODO@
+-- 'Transaction'.
 datedEntriesToTransactions :: Day -> CliOpts -> Journal -> [DatedEntry] -> [(DatedEntry, Transaction)]
 datedEntriesToTransactions today hledgerCliOpts journal datedEntries =
   let datedEntriesAndMaybeSimilarTransactions =
@@ -88,22 +113,53 @@ datedEntriesToTransactions today hledgerCliOpts journal datedEntries =
         fmap (uncurry nothingTransactionToTempTransaction) datedEntriesAndMaybeNewTransactions
   in datedEntriesAndTransactions
 
-nothingTransactionToTempTransaction :: DatedEntry -> Maybe Transaction -> (DatedEntry, Transaction)
-nothingTransactionToTempTransaction datedEntry (Just transaction) = (datedEntry, transaction)
-nothingTransactionToTempTransaction datedEntry Nothing = (datedEntry, createTempTransaction datedEntry)
+-- | Takes a 'DatedEntry' and a 'Maybe' 'Transaction'.  If the 'Maybe'
+-- 'Transaction' is 'Just', then just return a tuple of the 'DatedEntry' and
+-- the 'Transaction'.  If the 'Maybe' 'Transaction' is 'Nothing', then use
+-- 'createTempTransaction' to create a temporary @TODO@ 'Transaction' that
+-- needs to be filled in by hand.
+nothingTransactionToTempTransaction
+  :: DatedEntry
+  -> Maybe Transaction  -- ^ Possibly a real 'Transaction'.
+  -> (DatedEntry, Transaction) -- ^ Tuple of a 'DatedEntry' and either a real
+                               -- 'Transaction', or a temporary @TODO@
+                               -- 'Transaction' that needs to be filled in by
+                               -- hand.
+nothingTransactionToTempTransaction datedEntry maybeTransaction =
+  (datedEntry, fromMaybe (createTempTransaction datedEntry) maybeTransaction)
 
+-- | Uses 'datedEntryToMaybeSimilarTransaction' with the 'DatedEntry' passed
+-- in to find a 'Transaction' with a 'tdescription' similar to the
+-- 'DatedEntry's 'description'.  Returns both the 'DatedEntry' and the similar
+-- 'Transaction'.
 zipDatedEntryAndSimilarTransaction :: Day
                                    -> CliOpts
                                    -> Journal
                                    -> DatedEntry
                                    -> (DatedEntry, Maybe Transaction)
-zipDatedEntryAndSimilarTransaction today hledgerCliOpts journal =
-  applyFuncAndZip $
-  datedEntryToMaybeSimilarTransaction today hledgerCliOpts journal
+zipDatedEntryAndSimilarTransaction today hledgerCliOpts journal datedEntry =
+  let maybeSimilarTransaction =
+        datedEntryToMaybeSimilarTransaction
+          today
+          hledgerCliOpts
+          journal
+          datedEntry
+  in (datedEntry, maybeSimilarTransaction)
 
-zipDatedEntryAndNewTransaction :: DatedEntry
-                               -> Maybe Transaction
-                               -> (DatedEntry, Maybe Transaction)
+-- | In @'zipDatedEntryAndNewTransaction' datedEntry maybeSimilarTransaction@,
+-- if @maybeSimilarTransaction@ is 'Nothing', then just return
+-- @(datedEntry, Nothing)@.
+--
+-- But if @maybeSimilarTransaction@ is @'Just' similarTransaction@, then call
+-- 'datedEntryAndSimilarTransactionToRealTransaction' with @similarTransaction@
+-- in order to create a real 'Transaction' based on the similar 'Transaction'.
+zipDatedEntryAndNewTransaction
+  :: DatedEntry
+  -> Maybe Transaction -- ^ Similar transaction
+  -> (DatedEntry, Maybe Transaction) -- ^ Tuple of 'DataEntry' that was passed
+                                     -- in, and a real 'Transaction' based on
+                                     -- the 'DataEntry' and similar
+                                     -- 'Transaction' passed in.
 zipDatedEntryAndNewTransaction datedEntry Nothing = (datedEntry, Nothing)
 zipDatedEntryAndNewTransaction datedEntry (Just similarTransaction) =
   ( datedEntry
@@ -111,18 +167,30 @@ zipDatedEntryAndNewTransaction datedEntry (Just similarTransaction) =
       datedEntry
       similarTransaction)
 
-datedEntryToMaybeSimilarTransaction :: Day
-                                    -> CliOpts
-                                    -> Journal
-                                    -> DatedEntry
-                                    -> Maybe Transaction
+-- | Find a similar 'Transaction' for a given 'DatedEntry'. Uses
+-- 'getSimilarTransaction'.
+--
+-- If a similar transaction can't be found, return 'Nothing'.
+datedEntryToMaybeSimilarTransaction
+  :: Day -- ^ Today
+  -> CliOpts
+  -> Journal
+  -> DatedEntry -- ^ Use the 'description' from this to find a similar
+                -- 'Transaction'.
+  -> Maybe Transaction
 datedEntryToMaybeSimilarTransaction today hledgerCliOpts journal datedEntry =
   let entryDesc = (description :: DatedEntry -> Text) datedEntry
   in getSimilarTransaction today hledgerCliOpts journal entryDesc
 
-datedEntryAndSimilarTransactionToRealTransaction :: DatedEntry
-                                                 -> Transaction
-                                                 -> Maybe Transaction
+-- | Convert a 'DatedEntry' and a similar 'Transaction' to a real 'Transaction'.
+--
+-- If there is no @\"assets:cash\"@ and @\"expenses:*\"@ account names in the
+-- similar 'Transaction', then return 'Nothing'.
+datedEntryAndSimilarTransactionToRealTransaction
+  :: DatedEntry
+  -> Transaction -- ^ A 'Transaction' that is similar to the description in the
+                 -- 'DatedEntry'.
+  -> Maybe Transaction
 datedEntryAndSimilarTransactionToRealTransaction datedEntry similarTransaction =
   case paccount <$> tpostings similarTransaction of
     [account1, account2] -> do
@@ -131,7 +199,14 @@ datedEntryAndSimilarTransactionToRealTransaction datedEntry similarTransaction =
       pure $ createNewTransaction datedEntry assetsCashAccount expensesAccount
     _ -> Nothing
 
-createNewTransaction :: DatedEntry -> AccountName -> AccountName -> Transaction
+
+-- | Create a new 'Transaction' based on the 'DatedEntry' with the given
+-- 'AccountName's.
+createNewTransaction
+  :: DatedEntry
+  -> AccountName -- ^ This 'AccountName' should be @\"assets:cash\"@.
+  -> AccountName  -- ^ This 'AccountName' should start with @expenses@.
+  -> Transaction
 createNewTransaction datedEntry assetsCashAccount expensesAccount =
   let entryAmount = datedEntryAmountToQuantity datedEntry
       expensesAmount = num entryAmount
@@ -146,27 +221,37 @@ createNewTransaction datedEntry assetsCashAccount expensesAccount =
      , tpostings = [assetsCashPosting, expensesPosting]
      }
 
+-- | Create a temporary @TODO@ 'Transaction' that needs to filled in by hand
+-- based on this 'DatedEntry'.
 createTempTransaction :: DatedEntry -> Transaction
 createTempTransaction datedEntry =
   let transaction = createNewTransaction datedEntry "assets:cash" "expenses:_"
   in transaction {tcomment = "TODO this transaction"}
 
+-- | Turn a 'PositiveInt' into a 'Quantity'.
 positiveIntToQuantity :: PositiveInt -> Quantity
 positiveIntToQuantity = fromIntegral . unPositiveInt
 
+-- | Pull the 'amount' out of a 'DatedEntry' and turn it into a 'Quantity'.
 datedEntryAmountToQuantity :: DatedEntry -> Quantity
 datedEntryAmountToQuantity =
   positiveIntToQuantity . (amount :: DatedEntry -> PositiveInt)
 
+-- | Turn an 'AccountName' and an 'Amount' into a 'Posting'.  This is a simple
+-- way to create a 'Posting'.
 accountAndAmountToPosting :: AccountName -> Amount -> Posting
 accountAndAmountToPosting accountName amount =
   nullposting {paccount = accountName, pamount = Mixed [amount]}
 
+-- | Given two 'AccountName's, figure out which one is @\"assets:cash\"@.
+-- Return 'Nothing' if neither is.
 findAssetsCashAccountName :: AccountName -> AccountName -> Maybe AccountName
 findAssetsCashAccountName "assets:cash" _ = Just "assets:cash"
 findAssetsCashAccountName _ "assets:cash" = Just "assets:cash"
 findAssetsCashAccountName _ _ = Nothing
 
+-- | Given two 'AccountName's, figure out which one has a prefix of
+-- @\"expenses\"@.  Return 'Nothing' if neither does.
 findExpensesAccountName :: AccountName -> AccountName -> Maybe AccountName
 findExpensesAccountName account1 account2
   | "expenses" `isPrefixOf` account1 = Just account1
@@ -177,13 +262,16 @@ findExpensesAccountName account1 account2
 -- Parse Command Line Options --
 --------------------------------
 
+-- | Data type representing the command line options.
 data Options = Options
-  { inputFilePath :: FilePath
+  { inputFilePath :: FilePath -- ^ Input file path for the file that comes from
+                              -- Google Keep.
   } deriving (Data, Eq, Generic, Read, Show, Typeable)
 
 optionsParser :: Parser Options
 optionsParser = Options <$> argument str (metavar "INPUT_FILE")
 
+-- | Parse the command line arguments into 'Options'.
 parseCmdLineOptions :: MonadIO m => m Options
 parseCmdLineOptions = liftIO $ execParser opts
   where
@@ -272,16 +360,7 @@ dayParser today = do
         then "0" <> show int
         else show int
 
--- | calculate the year to use for an input month.  If it is currently January
--- or February, and the input month is November or December, then use the
--- previous year.  Otherwise, use the current year.
-calcYear :: Day -> Int -> Integer
-calcYear today month =
-  let (yearForToday, monthForToday, _) = toGregorian today
-  in if (monthForToday == 1 || monthForToday == 2) && (month == 11 || month == 12)
-       then yearForToday - 1
-       else yearForToday
-
+-- | Parse a 'PostiveInt'.
 positiveIntParser :: MyParser PositiveInt
 positiveIntParser = do
   digitsText <- pack <$> many1 digit <?> "Trying to parse a set of digits."
@@ -295,17 +374,27 @@ positiveIntParser = do
 eitherToMaybe :: Either e a -> Maybe a
 eitherToMaybe = either (const Nothing) Just
 
+-- | Print out the error message passed in and then call 'exitFailure'.
 die :: MonadIO m => Text -> m a
 die msg = do
   putStrLn $ "ERROR: " <> msg
   liftIO exitFailure
 
-applyFuncAndZip :: (a -> b) -> a -> (a, b)
-applyFuncAndZip f a = (a, f a)
-
+-- | Safe version of 'tail'.  Returns '[]' if the input list is empty.
 tailSafe :: [a] -> [a]
 tailSafe [] = []
 tailSafe (_:t) = t
+
+-- | Calculate the year to use for an input month.  If it is currently January
+-- or February, and the input month is November or December, then use the
+-- previous year.  Otherwise, use the current year.
+calcYear :: Day -> Int -> Integer
+calcYear today month =
+  let (yearForToday, monthForToday, _) = toGregorian today
+  in if (monthForToday == 1 || monthForToday == 2) && (month == 11 || month == 12)
+       then yearForToday - 1
+       else yearForToday
+
 
 -----------
 -- Types --
@@ -330,6 +419,8 @@ positiveInt inputText =
                 "input \"" <> inputText <>
                 "\" is less than 0. Not a positive int."
 
+-- | This is just a 'PostiveInt' 'amount' with a description.  Used in
+-- 'SimpleEntriesOnDate'.
 data SimpleEntry = SimpleEntry
   { amount :: PositiveInt
   , description :: Text
