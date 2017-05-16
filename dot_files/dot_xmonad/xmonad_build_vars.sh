ba73b17d5261e6e6681296a8d4ec88abd01db3a6
(cdep)illabout <cdep.illabout@gmail.com>
2017-05-17 02:15:27 +0900

    Update build script for xmonad.

--- dot_files/dot_xmonad/xmonad_build_vars.sh
+++ dot_files/dot_xmonad/xmonad_build_vars.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # Set config values used for building and running xmonad and taffybar.
 
 export XMONAD_DIR="${HOME}/.xmonad"
-export XMONAD_LOCAL_BIN_PATH="${xmonad_dir}/local-bin"
+export XMONAD_LOCAL_BIN_PATH="${XMONAD_DIR}/local-bin"
 export XMONAD_STACK_RESOLVER="lts-8.13"
