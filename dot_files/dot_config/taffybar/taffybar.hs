5f2121353a2a4f12d5220dcc7213c4adf4d02edc
(cdep)illabout <cdep.illabout@gmail.com>
2017-05-18 00:04:44 +0900

    Update some taffybar stuff to make it look better.

--- dot_files/dot_config/taffybar/taffybar.hs
+++ dot_files/dot_config/taffybar/taffybar.hs
@@ -1,14 +1,26 @@
 {-# OPTIONS_GHC -Wall #-}
 
+import System.Information.CPU (cpuLoad)
+import System.Information.Memory (parseMeminfo, memoryUsedRatio)
 import System.Taffybar
-       (defaultTaffybar, defaultTaffybarConfig, endWidgets, startWidgets)
+       (barHeight, defaultTaffybar, defaultTaffybarConfig, endWidgets,
+        startWidgets)
+import System.Taffybar.Battery (batteryBarNew, defaultBatteryConfig)
+-- import System.Taffybar.DiskIOMonitor (dioMonitorNew)
+import System.Taffybar.FreedesktopNotifications
+       (defaultNotificationConfig, notifyAreaNew)
 import System.Taffybar.Systray (systrayNew)
 import System.Taffybar.SimpleClock (textClockNew)
 import System.Taffybar.TaffyPager
        (defaultPagerConfig, taffyPagerNew)
 import System.Taffybar.Widgets.PollingGraph
        (defaultGraphConfig, graphDataColors, graphLabel, pollingGraphNew)
-import System.Information.CPU (cpuLoad)
+
+
+memCallback :: IO [Double]
+memCallback = do
+  mi <- parseMeminfo
+  pure [memoryUsedRatio mi]
 
 cpuCallback :: IO [Double]
 cpuCallback = do
@@ -22,16 +34,31 @@ main = do
           { graphDataColors = [ (0, 1, 0, 1), (1, 0, 1, 0.5) ]
           , graphLabel = Just "cpu"
           }
+      memCfg =
+        defaultGraphConfig
+          { graphDataColors = [(1, 0, 0, 1)]
+          , graphLabel = Just "mem"
+          }
+      -- diskCfg =
+      --   defaultGraphConfig
+      --     { {- graphDataColors = [(1, 0, 0, 1)]
+      --     , -} graphLabel = Just "disk"
+      --     }
       clock =
         textClockNew
           Nothing
           "<span fgcolor='orange'>%a %b %_d %H:%M</span>"
           1
       pager = taffyPagerNew defaultPagerConfig
+      note = notifyAreaNew defaultNotificationConfig
       tray = systrayNew
-      cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+      cpu = pollingGraphNew cpuCfg 2 cpuCallback
+      mem = pollingGraphNew memCfg 5 memCallback
+      -- disk = dioMonitorNew diskCfg 2 "sda"
+      battery = batteryBarNew defaultBatteryConfig 2
   defaultTaffybar
     defaultTaffybarConfig
-      { startWidgets = [pager]
-      , endWidgets = [tray, clock, cpu]
+      { barHeight = 20
+      , startWidgets = [pager, note]
+      , endWidgets = [tray, clock, battery, mem, cpu]
       }
