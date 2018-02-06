57b395008963bd11bba4ff73c224e61dc01d76c0
(cdep)illabout <cdep.illabout@gmail.com>
2018-02-06 17:49:25 -0500

    Update some fies.

--- dot_files/dot_xmonad/xmonad_build_vars.sh
+++ dot_files/dot_xmonad/xmonad_build_vars.sh
@@ -2,6 +2,11 @@
 #
 # Set config values used for building and running xmonad and taffybar.
 
+# The directory in $HOME with all the XMonad stuff.
 export XMONAD_DIR="${HOME}/.xmonad"
+
+# A directory to use to install the taffybar and xmonad binaries.
 export XMONAD_LOCAL_BIN_PATH="${XMONAD_DIR}/local-bin"
+
+# The lts resolver we will use for building with stack.
 export XMONAD_STACK_RESOLVER="lts-9.14"
