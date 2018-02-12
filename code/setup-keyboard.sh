8ed2136ebd6e6e134aebafb1dc08ab82867dc71b
(cdep)illabout <cdep.illabout@gmail.com>
2018-02-12 12:52:45 +0900

    Make sure that multiple versions of setup-keyboard cannot be run at once.

--- code/setup-keyboard.sh
+++ code/setup-keyboard.sh
@@ -1,21 +1,38 @@
 #!/usr/bin/env bash
-
+#
 # Setup the keyboard map based on what keyboard I have plugged in.
+#
+# We need to use a lock to make sure that this doesn't get run twice at once.
+# This is because setxkbmap needs to be called before xmodmap, and xmodmap can
+# only be called ONCE after setxkbmap.  This is because it switches the current
+# values of the keys, instead of making sure they are the exact same everytime.
 
-echo "xauth file: $XAUTHORITY" >> /tmp/trying-to-setup-keyboard
+LOCKFILE=/tmp/setup-keyboard.lock
 
-setxkbmap us
-
-if [ -f "$HOME/.Xmodmap_swap_ctrl_caps" ]; then
-	xmodmap "$HOME/.Xmodmap_swap_ctrl_caps"
+# Create the lockfile if it does not exist.
+if [ ! -f "${LOCKFILE}" ] ; then
+  touch "${LOCKFILE}"
 fi
 
-# change keys on Japanese keyboard if we are using a Japanese keyboard
-japanese_keyboard_xmodmap="$HOME/.Xmodmap_japanese_keyboard"
+(
+  flock -n 200 || exit 1
 
-if [ -f "$japanese_keyboard_xmodmap" ]; then
-	grep_output="$(lsusb | grep '045e:00dc Microsoft Corp.')"
-	if [ -n "$grep_output" ] ; then
-		xmodmap "$japanese_keyboard_xmodmap"
-	fi
-fi
+  sleep 0.5
+
+  setxkbmap -display "${DISPLAY}" us
+
+  if [ -f "$HOME/.Xmodmap_swap_ctrl_caps" ]; then
+    xmodmap -display "${DISPLAY}" "$HOME/.Xmodmap_swap_ctrl_caps"
+  fi
+
+  # change keys on Japanese keyboard if we are using a Japanese keyboard
+  japanese_keyboard_xmodmap="$HOME/.Xmodmap_japanese_keyboard"
+
+  if [ -f "$japanese_keyboard_xmodmap" ]; then
+    lsusb_output="$(lsusb -d "045e:00dc")"
+    if [ -n "$lsusb_output" ] ; then
+      xmodmap "$japanese_keyboard_xmodmap"
+    fi
+  fi
+
+) 200<"${LOCKFILE}"
