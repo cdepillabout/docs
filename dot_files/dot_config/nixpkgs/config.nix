c26299f198ae49742d3e9bc483a359e76a472c53
(cdep)illabout <cdep.illabout@gmail.com>
2018-03-02 13:03:44 +0900

    Added some additional environments to the config.nix file.

--- dot_files/dot_config/nixpkgs/config.nix
+++ dot_files/dot_config/nixpkgs/config.nix
@@ -1,4 +1,25 @@
-
+with (import <nixpkgs> {});
 {
   allowUnfree = true;
+
+  # The package sets defined here can be installed with `nix-env` like the
+  # following:
+  # $ nix-env -f '<nixpkgs>' -iA screenshotToClipboardEnv
+  packageOverrides = pkgs: with pkgs; rec {
+    # This python environment is used for running the copy-image-to-clipboard
+    # program, which is called by the screenshot-to-clipboard program.
+    copyImageToClipboardEnv = python27.withPackages (ps: with ps; [
+        pygtk
+      ]);
+
+    # This environment is used for running the screenshot-to-clipboard program.
+    screenshotToClipboardEnv = pkgs.buildEnv {
+      name = "screenshot-to-clipboard-env";
+      paths = [
+        copyImageToClipboardEnv
+        imagemagick
+        gimp
+      ];
+    };
+  };
 }
