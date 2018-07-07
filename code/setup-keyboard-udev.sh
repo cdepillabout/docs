d5dc7d236b585e5d4ebe0e4ea6d8f33efb1f9147
(cdep)illabout <cdep.illabout@gmail.com>
2018-07-08 00:54:38 +0900

    Update the setup-keyboard-udev script to not run when the X server is not running.

--- code/setup-keyboard-udev.sh
+++ code/setup-keyboard-udev.sh
@@ -11,8 +11,9 @@ export DISPLAY=":0.0"
 export XAUTHORITY="${HOME}/.Xauthority"
 
 if ps auxw | grep -i [x]org-server >/dev/null; then
-	echo "`date`: running setup-keyboard-udev.sh..." >> /tmp/setup-keyboard-udev.debug
+	# echo "`date`: running setup-keyboard-udev.sh..." >> /tmp/setup-keyboard-udev.debug
 	$CURDIR/setup-keyboard.sh & disown -h
 else
-	echo "`date`: not running setup-keyboard-udev.sh because the X server is not running" >> /tmp/setup-keyboard-udev.debug
+	:
+	# echo "`date`: not running setup-keyboard-udev.sh because the X server is not running" >> /tmp/setup-keyboard-udev.debug
 fi
