dacf9aacbe481363b8066c5672076aea60cf9ef9
(cdep)illabout <cdep.illabout@gmail.com>
2019-09-19 11:54:00 +0900

    Make sure arbtt doesnt record window titles.

--- dot_files/dot_config/nixpkgs/config.nix
+++ dot_files/dot_config/nixpkgs/config.nix
@@ -28,5 +28,21 @@
       addBuildDepend
         (appendConfigureFlag pkgs.haskellPackages.pretty-simple "-fbuildexe")
         pkgs.haskellPackages.optparse-applicative;
+
+    # Change arbtt so it doesn't record window titles.
+    arbtt =
+      pkgs.haskell.lib.overrideCabal pkgs.haskellPackages.arbtt (oldAttrs: {
+        postPatch = (oldAttrs.postPatch or "") + ''
+          pattern="getWindowTitle dpy =  myFetchName dpy"
+          replace='getWindowTitle _ _ = pure ""'
+          file="src/Capture/X11.hs"
+          if grep -q "$pattern" "$file"; then
+            substituteInPlace "$file" --replace "$pattern" "$replace"
+          else
+            echo "pattern \`$pattern\` does not exist in $file, so not able to replace it with \`$replace\`"
+            exit 1
+          fi
+        '';
+      });
   };
 }
